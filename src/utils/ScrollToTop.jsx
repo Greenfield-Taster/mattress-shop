7084f106196db8dc1418b3f409939758a7d8641a
Anastasiia Horbachova <77613323+Greenfield-Taster@users.noreply.github.com>
2025-11-26 16:28:11 +0200

    Add sctoll to top in all new pages

--- src/utils/ScrollToTop.jsx
+++ src/utils/ScrollToTop.jsx
@@ -1,16 +1,21 @@
 import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigationType } from 'react-router-dom';
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
+  const navigationType = useNavigationType();
 
   useEffect(() => {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: 'smooth'
-    });
-  }, [pathname]);
+    // Скролимо вгору тільки при PUSH (нова сторінка) або REPLACE
+    // Не скролимо при POP (кнопка назад/вперед браузера)
+    if (navigationType !== 'POP') {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'instant'
+      });
+    }
+  }, [pathname, navigationType]);
 
   return null;
 };
